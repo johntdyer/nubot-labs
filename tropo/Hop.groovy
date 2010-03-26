12f5137dbf1fcb3a6e203e32f761951b7f98dbd1
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-03-26 10:38:19 -0400

    change redirect sip url

--- tropo/Hop.groovy
+++ tropo/Hop.groovy
@@ -1,4 +1,6 @@
 
 log("redirecting to sip:9991437537@sip-prod.voxeo.net");
 
-redirect("sip:9991437537@sip-prod.voxeo.net");
+redirect("sip:5159406815@glem.dyndns.org");
+//redirect("sip:9991437537@sip-prod.voxeo.net");
+
