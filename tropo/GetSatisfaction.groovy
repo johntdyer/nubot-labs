47faf6b0daa63d34f7b5fb92dedd1d428b43a470
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-03-25 20:53:02 -0400

    add dtmf sequencer mechanism for generating dtmf sequence

--- tropo/GetSatisfaction.groovy
+++ tropo/GetSatisfaction.groovy
@@ -3,15 +3,25 @@
 // See http://www.tropo.com for more info
 // --------------------------------------------
 
-answer();
-
 def baseAudioUrl = "http://github.com/pdeschen/nubot-labs/raw/master/audio";
+def dtmfSequencerEnabled = true;
+
+def isay = { text, sequence ->
+
+	if (dtmfSequencerEnabled)
+	{	
+		for (dtmf in sequence) {
+			say("${baseAudioUrl}/dtmf/${dtmf}.wav");
+		}
+	}
+	say (text);
+}
+
+answer();
 
 say("${baseAudioUrl}/intro.wav");
 
-say("Thank you for calling the Travel Agency Customer Satisfaction Department");
-
-say("${baseAudioUrl}/dtmf/a.wav");
+isay("Thank you for calling the Travel Agency Customer Satisfaction Department", "a");
 
 result=ask( "Did you purchase Package A, Package B, or Package C?", 
 			[choices:"a( 1, package a), b( 2, package b), c(3, package c)"] );
