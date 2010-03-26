c6ba59f6a8952f89e1350f666dbfae16c7de63d7
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-03-26 10:48:06 -0400

    change phone number typo

--- tropo/Hop.groovy
+++ tropo/Hop.groovy
@@ -1,6 +1,6 @@
 
 log("redirecting to sip:9991437537@sip-prod.voxeo.net");
 
-redirect("sip:5159406815@glem.dyndns.org");
+redirect("sip:5149406815@glem.dyndns.org");
 //redirect("sip:9991437537@sip-prod.voxeo.net");
 
