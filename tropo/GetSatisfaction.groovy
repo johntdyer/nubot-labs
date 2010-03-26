0dcb3c9507ed5061fd8bb42f9c413e20819a60a3
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-03-25 21:08:53 -0400

    now using a sequencer closure

--- tropo/GetSatisfaction.groovy
+++ tropo/GetSatisfaction.groovy
@@ -6,7 +6,7 @@
 def baseAudioUrl = "http://github.com/pdeschen/nubot-labs/raw/master/audio";
 def dtmfSequencerEnabled = true;
 
-def isay = { text, sequence ->
+def sequencer = { sequence, closure ->
 
 	if (dtmfSequencerEnabled)
 	{	
@@ -14,23 +14,25 @@ def isay = { text, sequence ->
 			say("${baseAudioUrl}/dtmf/${dtmf}.wav");
 		}
 	}
-	say (text);
+	if (closure) {return closure() }
 }
 
 answer();
 
 say("${baseAudioUrl}/intro.wav");
 
-isay("Thank you for calling the Travel Agency Customer Satisfaction Department", "a");
+sequencer("a") { say("Hello 1. Thank you for calling the Travel Agency Customer Satisfaction Department") };
 
 result=ask( "Did you purchase Package A, Package B, or Package C?", 
 			[choices:"a( 1, package a), b( 2, package b), c(3, package c)"] );
 
 if (result.name=='choice')
 {
-	if (result.value=="a") { say( "Overall, were you satisfied with the service you received while shopping for package A") }
-	if (result.value=="b") { say( "Overall, were you satisfied with the service you received while shopping for package A") }
-	if (result.value=="c") { say( "Overall, were you satisfied with the service you received while shopping for package A") }
+	if (result.value=="a") { 
+		sequencer("c20") { say( "Overall, were you satisfied with the service you received while shopping for package A" ) }
+	}
+	if (result.value=="b") { sequencer("c30") { say( "Overall, were you satisfied with the service you received while shopping for package B") } }
+	if (result.value=="c") { sequencer("c40") { say( "Overall, were you satisfied with the service you received while shopping for package C" ) } }
 }
 
 hangup();
