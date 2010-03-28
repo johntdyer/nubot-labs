8734034f4a048cbfada96a5e0cf196bbc5044dec
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-03-27 22:17:16 -0400

    revert sequencer + change event scope

--- tropo/src/BasicAttendant.groovy
+++ tropo/src/BasicAttendant.groovy
@@ -60,31 +60,42 @@ def listOptions(theContacts)
 }
 
 def contacts = [
-        "jonathan": [ nameChoices: "Jonathan, Jonathan Taylor", number: "1" ],
-        "michael" : [ nameChoices: "Michael, Michael Smith", number: "1" ],
-        "stephen" : [ nameChoices: "Stephen, Stephen Neish", number: "1" ] ]
+        "jonathan": [ nameChoices: "Jonathan, Jonathan Taylor", number: "14074434233" ],
+        "michael" : [ nameChoices: "Michael, Michael Smith", number: "14074181800" ],
+        "stephen" : [ nameChoices: "Stephen, Stephen Neish", number: "14076463131" ] ]
 
 
 answer( 30 )
-say( "Hello, and thank you for calling." )
+sequencer ("C10")
+        { say( "Hello, and thank you for calling." ) }
 def text = "Who would you like to call? Just say " + listNames( contacts )
 
-def event = prompt(text, [
-        repeat:3, 
-        timeout:7, 
-        choices:listOptions( contacts ), 
-        onEvent:
-        { event->
-            event.onTimeout(
-            {  say( "I'm sorry, I didn't hear anything." )   } )
-            event.onBadChoice(
-                    { say( "I'm sorry, I didn't understand what you said." )  } )
+def event;
+
+sequencer ("C20")
+        {
+            event = prompt(text, [
+            repeat:3, 
+            timeout:7, 
+            choices:listOptions( contacts ), 
+            onEvent:
+            { handlingEvent->
+                handlingEvent.onTimeout(
+                {
+                    sequencer ("C21")
+                    {say( "I'm sorry, I didn't hear anything." ) } } )
+                handlingEvent.onBadChoice(
+                        {
+                            sequencer ("C22")
+                            {say( "I'm sorry, I didn't understand what you said." ) } } )
+            }
+            ]
+            )
         }
-        ]
-        )
 // If a choice was made, transfer to that person
 if (event.name == "choice") {
-    say( "Ok, you said " + event.value + ". Please hold while I transfer you." )
+    sequencer ("C30")
+            { say( "Ok, you said " + event.value + ". Please hold while I transfer you." ) };
     
     def ne = transfer( "sip:9${contacts[ event.value ].number}@10.6.63.201", [
             answerOnMedia: false,
@@ -108,7 +119,8 @@ if (event.name == "choice") {
     log "transfer event.name  = $ne.name"
     log "transfer event.value = $ne.value"
     
-    say "The other party disconnected.  Goodbye"  
+    sequencer ("C90")
+            { say "The other party disconnected.  Goodbye" } 
 }
 
 hangup()
