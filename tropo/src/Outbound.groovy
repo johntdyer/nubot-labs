857fa556552211c1c2e9fe7bd0393e0e44342e87
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-03-31 19:30:44 -0400

    trying something else

--- tropo/src/Outbound.groovy
+++ tropo/src/Outbound.groovy
@@ -2,8 +2,8 @@
 answer();
 
 // Place a phone number here
-//def phoneNo = 5147657222
-def phoneNo = 5142220264
+def phoneNo = 5147657222
+//def phoneNo = 5142220264
 
 event = call("sip:${phoneNo}@10.6.63.201", 
         [
@@ -24,8 +24,8 @@ if(event.name=='answer')
 {
     newCall = event.value;
     //say()
-    await(1000)
-    result = newCall.ask("http://github.com/pdeschen/nubot-labs/raw/master/audio/dtmf/9.wav", [choices:"ok(hello my name is pascal)"])
+    await(5000)
+    result = newCall.ask("http://github.com/pdeschen/nubot-labs/raw/master/audio/dtmf/9.wav", [choices:"ok(for tomorrow the no parking regulation has been lifteds)"])
     
     //log(result.choice.xml);
     //log(result.choice.confidence);
