c402463616a293bea69df152eef3bc7c77bd2dc4
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-05-28 14:40:10 -0400

    add grab for testing purpose

--- tropo/src/GetSatisfaction.groovy
+++ tropo/src/GetSatisfaction.groovy
@@ -1,4 +1,7 @@
 // --------------------------------------------
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0' )
+import groovyx.net.http.* 
+
 // using both speech and touch-tone input
 // See http://www.tropo.com for more info
 // --------------------------------------------
@@ -11,17 +14,18 @@ def dtmfSequencerEnabled = true;
 
 log("dnis: " + currentCall.calledID);
 
-if (currentCall.calledID == "6172977902") { dtmfSequencerEnabled = false; }
+if (currentCall.calledID == "6172977902") { dtmfSequencerEnabled = false;
+}
 
 def sequencer = { sequence, closure ->
-
-	if (dtmfSequencerEnabled)
-	{	
-		for (dtmf in sequence) {
-			say("${baseAudioUrl}/dtmf/${dtmf}.wav");
-		}
-	}
-	if (closure) {return closure() }
+  
+  if (dtmfSequencerEnabled) {	
+    for (dtmf in sequence) {
+      say("${baseAudioUrl}/dtmf/${dtmf}.wav");
+    }
+  }
+  if (closure) {return closure()
+  }
 }
 
 
@@ -31,32 +35,33 @@ say("${baseAudioUrl}/intro.wav");
 
 sequencer("a") { say("Hello 1. Thank you for calling the Travel Agency Customer Satisfaction Department") };
 
-sequencer("c10") { result=ask( "Did you purchase Package A, Package B, or Package C?", 
-			[choices:"a( 1, package a), b( 2, package b), c(3, package c)"] ) };
+sequencer("c10") {
+  result=ask( "Did you purchase Package A, Package B, or Package C?", 
+  [choices:"a( 1, package a), b( 2, package b), c(3, package c)"] )
+};
 
-if (result.name=='choice')
-{
-	if (result.value=="a") { 
-		sequencer("c20") { 
-			result = ask( "Overall, were you satisfied with the service you received while shopping for package A" , [choices: '[BOOLEAN]']) 
-			if (result.value)
-			{
-				sequencer("c92") { say ('''
+if (result.name=='choice') {
+  if (result.value=="a") { 
+    sequencer("c20") { 
+      result = ask( "Overall, were you satisfied with the service you received while shopping for package A" , [choices: '[BOOLEAN]']) 
+      if (result.value) {
+        sequencer("c92") { say ('''
 					Thank you for your feedback. We are so pleased to provide you with 
 					satisfactory service today. We hope we will be a part of all your future 
 					travel booking plans! Goodbye.''')}
-			}
-			else
-			{
-                                sequencer("c90") { say ('''
+      }
+      else {
+        sequencer("c90") { say ('''
 					Your opinion is very important to us. Let me transfer you to an agent who 
 					will try to resolve any issues you had during the booking process. Please hold...
 					''')}
-			}
-		}
-	}
-	if (result.value=="b") { sequencer("c30") { say( "Overall, were you satisfied with the service you received while shopping for package B") } }
-	if (result.value=="c") { sequencer("c40") { say( "Overall, were you satisfied with the service you received while shopping for package C" ) } }
+      }
+    }
+  }
+  if (result.value=="b") { sequencer("c30") { say( "Overall, were you satisfied with the service you received while shopping for package B")
+    } }
+  if (result.value=="c") { sequencer("c40") { say( "Overall, were you satisfied with the service you received while shopping for package C" )
+    } }
 }
 
 hangup();
