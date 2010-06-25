418199501dfc2e76ce3240e45372b479062bf637
Pascal Deschenes <pascal.deschenes@nuecho.com>
2010-06-25 12:34:01 -0400

    redirect home

--- tropo/src/Hop.groovy
+++ tropo/src/Hop.groovy
@@ -1,4 +1,6 @@
 log("redirecting to sip:9991437537@sip-prod.voxeo.net");
 
-redirect("sip:5149406815@glem.dyndns.org");
+
+redirect("sip:5149406815@pdeschen.no-ip.org");
+//redirect("sip:5149406815@glem.dyndns.org");
 //redirect("sip:9991437537@sip-prod.voxeo.net");
